265c4f9d31f6bdb08d9a18e390a6869fc5422482
Roman Tatarin <romantatarin@MacBook-Air-Roman.local>
2025-09-20 23:21:05 +1100

    css corrections

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# ----------------------------
+# Активируем виртуальное окружение
+# ----------------------------
+# точка (.) = source, подгружает окружение в текущий shell
+. /opt/venv/bin/activate
+
 echo "⏳ Ожидание запуска Postgres..."
 while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   sleep 1
@@ -15,4 +21,3 @@ python manage.py collectstatic --noinput
 
 echo "Запускаем WSGI сервер..."
 gunicorn stolichny.wsgi:application --bind 0.0.0.0:8000 --workers 3
-
